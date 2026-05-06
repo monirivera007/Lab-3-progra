d971c9b335c27496e71a35dc9e259b7d25778e8a
monirivera007 <mmrcarpio07@gmail.com>
2026-05-05 20:43:55 -0600

    nuevo

--- lab 3.py
+++ lab 3.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 opcion = st.sidebar.selectbox(
     "selecciona una opción",
-    ["Vehículos"]
+    ["Vehículos", "Gimnasio", "Videojuegos", "Netflix"]
 
 )
 
@@ -88,3 +88,89 @@ if opcion == "Vehículos":
         
     st.write(grupo)
 
+gimnasio =pd.read_csv("GymExerciseTracking.csv")
+
+if opcion == "Gimnasio":
+
+    st.title("Analisis de Gimnasio")
+
+    st.subheader("Dimensiones del dataset")
+    st.write(gimnasio.shape)
+
+    st.subheader("Columnas")
+    st.write(gimnasio.columns.tolist())
+
+    st.subheader("Primeras 6 filas")
+    st.dataframe(gimnasio.head(6))
+
+    st.subheader("Estadísticas")
+    st.write(gimnasio.describe())
+
+    st.subheader("Filtro por calorías")
+
+    calorias = st.number_input(
+        "Mostrar registros con calorías >= a:",
+        min_value=0.0
+
+    )
+
+    filtro_calorias = gimnasio[gimnasio["Calories_Burned"] >= calorias]
+
+    st.write(filtro_calorias)
+    
+    st.subheader("Filtro por porcentaje de grasa")
+
+    grasa = st.number_input(
+        "mostrar registros con grasa <= a:",
+        min_value=0.0,
+        max_value=100.0
+
+    )
+
+    filtro_grasa = gimnasio[gimnasio["Fat_Percentage"] <= grasa]
+
+    st.write(filtro_grasa)
+
+    def nivel_frecuencia(x):
+        if x < 3: 
+            return "Baja"
+        elif x <= 5:
+            return "Moderada" 
+        else:
+            return "Alta"
+
+    gimnasio["NivelFrecuencia"] = gimnasio["Workout_Frequency (days/week)"].apply(nivel_frecuencia)
+
+    st.subheader("Conteo por frecuencia")
+
+    conteo = gimnasio["NivelFrecuencia"].value_counts()
+
+    st.write(conteo)
+
+    st.subheader("Gráfico de frecuencia")
+
+    st.bar_chart(conteo)
+
+    st.subheader("Análisis agrupado")
+
+    grupo = gimnasio.groupby("NivelFrecuencia").agg({
+        "Session_Duration (hours)": "mean",
+        "Experience_Level": "mean",
+        "BMI": "std"
+    })
+
+    st.write(grupo)
+
+    st.subheader("Relación calorías vs duración")
+
+    correlacion = gimnasio[["Calories_Burned", "Session_Duration (hours)"]].corr()
+
+    st.write(correlacion)
+
+    st.subheader("Relación grasa vs experiencia")
+
+    correlacion2 = gimnasio[["Fat_Percentage", "Experience_Level"]].corr()
+
+    st.write(correlacion2)
+
+    gimnasio.to_csv("GymExerciseTracking_Actualizado.csv", index=False)
